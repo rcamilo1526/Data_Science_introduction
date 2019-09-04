f4e0a12eda7619390dcfbc83d9ab463fb98fe2fb
rcamilomartin <rcamilo152622@gmail.com>
2019-09-03 21:17:14 -0500

    ejercicios con imagenes completos (el de filtros y el de desordenar una imagen dividida en 16

---
+++ Imagen/desordenar/imagendividir.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 20:52:13 2019
+
+@author: rcami
+"""
+
+from PIL import Image
+import numpy.matlib
+import numpy as np
+import cv2
+import copy
+
+orden=(np.arange(16))
+np.random.shuffle(orden)
+desorden=copy.deepcopy(orden)
+orden.sort()
+im=cv2.imread("16.jpg",0)
+
+x= int(im.shape[0]/4)
+y= int(im.shape[1]/4)  
+im2=np.zeros((x*4,y*4),dtype=int)
+im3=np.zeros((x*4,y*4),dtype=int)
+j=0
+while j<16:
+    for i in desorden:
+#        print(j,i)
+        im2[j//4*x:j//4*x+x,(j%4)*y:(j%4)*y+y]=im[i//4*x:i//4*x+x,(i%4)*y:(i%4)*y+y]
+        j+=1
+
+#guarda la nueva imagen y la muestra    
+cv2.imwrite('imagendesordenada.jpg',im2)
+imagendesordenada = Image.open("imagendesordenada.jpg")
+imagendesordenada.show()
+print('ordenar otra vez')
+i=0
+while i<16:
+    for j in desorden:
+#        print(i,j)
+        im3[j//4*x:j//4*x+x,(j%4)*y:(j%4)*y+y]=im2[i//4*x:i//4*x+x,(i%4)*y:(i%4)*y+y]
+        i+=1
+
+cv2.imwrite('imagenreordenada.jpg',im3)
+imagenreordenada = Image.open("imagenreordenada.jpg")
+imagenreordenada.show()
+    
---
+++ Imagen/filtro/Imagen.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from PIL import Image
+import numpy.matlib
+import numpy as np
+import cv2
+
+
+def filtro(x):
+    #Lee y muestra la imagen con PIL
+    imagenoriginal = Image.open("images.jpg")
+    imagenoriginal.show()
+    
+    im=cv2.imread("images.jpg",0)
+    #ya con el indicativo 0 lo toma en blanco y negro y se muestra
+    cv2.imwrite('imagesbn.jpg',im)
+    imagenbn = Image.open("imagesbn.jpg")
+    imagenbn.show()
+    #crear la nueva matriz a la cual se le van a asignar los valores de imagen
+    im2=np.zeros((im.shape[0], im.shape[1]),dtype=int)
+    i=0
+    #recorrer todas las matriz haciendo una matriz m de los elementos al rededor en caso de estar en una esquina toma como si la imagen continuara
+    while i<im2.shape[0]:
+        j=0
+        while j<im2.shape[1]:
+            m=np.array([[im[i-1,j-1],im[i-1,j],im[i-1,(j+1)%im2.shape[1]]],[im[i,j-1],im[i,j],im[i,(j+1)%im2.shape[1]]],[im[(i+1)%im2.shape[0],j-1],im[(i+1)%im2.shape[0],j],im[(i+1)%im2.shape[0],(j+1)%im2.shape[1]]]])
+            im2[i,j]=(sum(sum(m*x)))/9
+            j+=1
+        i+=1
+    #guarda la nueva imagen y la muestra    
+    cv2.imwrite('imagesf.jpg',im2)
+    imagenfinal = Image.open("imagesf.jpg")
+    imagenfinal.show()
+    
+pa=np.array([[-1,-1,-1],[-1,16,-1],[-1,-1,-1]])
+pb=np.array([[1,1,1],[1,1,1],[1,1,1]])
+
+filtro(pa)
